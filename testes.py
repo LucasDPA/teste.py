d4379e860b9dfdc577b5bf50fc28f9d775a1c1fb
lucas <lucas_msn_OS@hotmail.com>
2023-01-23 22:29:51 -0300

    primeiro_commit

---
+++ testes.py
@@ -0,0 +1,12 @@
+input = [
+    {'name': 'Maria Neusa de Aparecida',
+    'cpf': '97905796671',
+    'state': 'Sao Paulo',
+    'value': '1234' },
+    {'name': 'Ricardo Fontes',
+    'cpf': '44010762900',
+    'state': 'Rio Grande do Sul',
+    'value': 567},
+]
+def solucao(*args):
+
